022d1a05456e541d40f595bcf6cbe063d753c7e2
Ariel Goldblatt <a.k.a.gldbltt@gmail.com>
2012-12-02 15:41:47 -0800

    I'm going to kill a bitch

--- src/com/tyzoid/jailr/models/DatabaseEngine.java
+++
@@ -1,5 +0,0 @@
-package com.tyzoid.jailr.models;
-
-public enum DatabaseEngine {
-    MYSQL, H2
-}
--- src/com/tyzoid/jailr/util/Time.java
+++
@@ -1,7 +0,0 @@
-package com.tyzoid.jailr.util;
-
-public class Time {
-    public static int getTime() {
-        return (int) (System.currentTimeMillis() / 1000L);
-    }
-}
