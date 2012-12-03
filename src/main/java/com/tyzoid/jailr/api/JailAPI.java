3dd569384ee9541e887f4e0bc8eb9a9f4888ccdf
messageofDEATH <messageofdeath@live.com>
2012-12-03 17:16:35 -0600

    Added the rest of the methods. They are unimplemented.

--- src/main/java/com/tyzoid/jailr/api/JailAPI.java
+++ src/main/java/com/tyzoid/jailr/api/JailAPI.java
@@ -1,5 +1,7 @@
 package com.tyzoid.jailr.api;
 
+import java.util.ArrayList;
+
 import com.tyzoid.jailr.models.Meta;
 import com.tyzoid.jailr.serialization.LocationSerializer;
 import org.bukkit.Location;
@@ -19,5 +21,32 @@ public class JailAPI {
         Meta.removeWhere("key='unJailPoint'");
         Meta unJailPointModel = new Meta("unJailPoint", locationSerializer.getString());
         unJailPointModel.save();
+    }
+    
+    //TODO Implement setJail | this is called after isJailed() is called and it is returned false | This jails a player
+    public static void setJail() {
+    	
+    }
+    
+    //TODO Implement setUnJail | this is called after isJailed() is called and it is returned true | This unjails a player
+    public static void setUnJail() {
+    	
+    }
+    
+    //TODO Implement isJailed | Returns a boolean if the player is jailed
+    public static boolean isJailed() {
+    	
+    	return false;
+    }
+    
+    // TODO Implement getRemainingJailTime | this is called after isJailed() is called and it is returned true |void needs to be changed to appropriate variable
+    public static void getRemainingJailTime() {
+    	
+    }
+    
+    //TODO Implement getJailMates | Returns a list of all jailed players by name to list them on comamnd
+    public static ArrayList<String> getJailMates() {
+    	
+    	return null;
     }
 }
