d2c8dbc61e1dae1abd186facae975dd2e00bf96b
Ariel Goldblatt <a.k.a.gldbltt@gmail.com>
2012-12-03 19:45:45 -0800

    listener for keeping jailed players in jail

---
+++ src/main/java/com/tyzoid/jailr/events/WardenListener.java
@@ -0,0 +1,34 @@
+package com.tyzoid.jailr.events;
+
+import com.tyzoid.jailr.api.JailAPI;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+import java.util.List;
+
+/**
+ * Class that implements event handlers that prevent prisoners
+ * from escaping jail.
+ *
+ * @author Sushi
+ */
+public class WardenListener implements Listener {
+    @EventHandler
+    public void onSpawn(PlayerRespawnEvent event) {
+        Player player = event.getPlayer();
+        if (JailAPI.isJailed(player.getName())) {
+            player.teleport(JailAPI.getJailPoint());
+        }
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        if (JailAPI.isJailed(player.getName())) {
+            player.teleport(JailAPI.getJailPoint());
+        }
+    }
+}
